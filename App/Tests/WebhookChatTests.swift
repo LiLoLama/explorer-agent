17b289a8cba4c839c00fa25ba12b380ab798e882
LiLoLama <mail@liamschmid.com>
2025-09-19 11:20:34 +0200

    Delete App directory

--- App/Tests/WebhookChatTests.swift
+++
@@ -1,9 +0,0 @@
-import XCTest
-@testable import WebhookChat
-
-final class WebhookChatTests: XCTestCase {
-    func testAppEntryPointIsAvailable() throws {
-        let app = ExplorerAgentApp()
-        _ = app.body
-    }
-}
